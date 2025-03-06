ae1572d124214e035e7304a2b22654fd9f47a2f9
boxp <tiyotiyouda@gmail.com>
2025-03-06 22:58:25 +0900

    fix(cloudflare/boxp.tk/portfolio): リダイレクトルールのターゲットパスを修正

--- terraform/cloudflare/boxp.tk/portfolio/dns.tf
+++ terraform/cloudflare/boxp.tk/portfolio/dns.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_page_rule" "redirect_top" {
   zone_id  = var.zone_id        # ドメインのzone ID
-  target   = "https://boxp.tk*" # リダイレクト元
+  target   = "https://boxp.tk/*" # リダイレクト元
   priority = 2                  # ルールの優先度
 
   actions {
@@ -13,7 +13,7 @@ resource "cloudflare_page_rule" "redirect_top" {
 
 resource "cloudflare_page_rule" "redirect_www" {
   zone_id  = var.zone_id            # ドメインのzone ID
-  target   = "https://www.boxp.tk*" # リダイレクト元
+  target   = "https://www.boxp.tk/*" # リダイレクト元
   priority = 1                      # ルールの優先度
 
   actions {
