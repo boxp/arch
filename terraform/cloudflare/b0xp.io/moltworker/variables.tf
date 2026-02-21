69c450612bba91df3b68fe2eea974d5bf7c5e87a
Keitaro Takeuchi <tiyotiyouda@gmail.com>
2026-02-21 10:48:10 +0900

    resurrect r2 (#7124)
    Co-authored-by: boxp-tfaction[bot] <162872338+boxp-tfaction[bot]@users.noreply.github.com>

--- terraform/cloudflare/b0xp.io/moltworker/variables.tf
+++ terraform/cloudflare/b0xp.io/moltworker/variables.tf
@@ -1,8 +1,8 @@
-# variable "account_id" {
-#   description = "The account ID of the Cloudflare account."
-#   type        = string
-#   default     = "1984a4314b3e75f3bedce97c7a8e0c81"
-# }
+variable "account_id" {
+  description = "The account ID of the Cloudflare account."
+  type        = string
+  default     = "1984a4314b3e75f3bedce97c7a8e0c81"
+}
 
 variable "zone_id" {
   description = "The zone ID of the b0xp.io zone."
