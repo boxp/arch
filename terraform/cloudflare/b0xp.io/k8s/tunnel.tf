55945d94f14500c58d68ebf598844a89bc53ae40
Keitaro Takeuchi <tiyotiyouda@gmail.com>
2026-05-22 21:22:26 +0900

    BOXP-7: Route Codex workspace via LoadBalancer IP (#9472)

--- terraform/cloudflare/b0xp.io/k8s/tunnel.tf
+++ terraform/cloudflare/b0xp.io/k8s/tunnel.tf
@@ -29,6 +29,6 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "k8s_tunnel" {
 resource "cloudflare_zero_trust_tunnel_route" "codex_workspace" {
   account_id = var.account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.id
-  network    = "10.111.250.7/32"
-  comment    = "lolice codex-workspace Service for WARP access"
+  network    = "192.168.10.98/32"
+  comment    = "lolice codex-workspace LoadBalancer for WARP access"
 }
